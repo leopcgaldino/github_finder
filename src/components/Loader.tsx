30cd467f46ea6b79101d22ec8c2be83fd0c21529
Leonardo Galdino <leonardogaldino721@gmail.com>
2022-12-31 17:49:51 -0300

    projeto final

---
+++ src/components/Loader.tsx
@@ -0,0 +1,13 @@
+import { FaSpinner } from "react-icons/fa";
+
+import classes from "./Loader.module.css";
+
+const Loader = () => {
+  return (
+    <>
+      <FaSpinner />
+    </>
+  );
+};
+
+export default Loader;
